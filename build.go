1f865f275a42e9435cfcc51ec6fba0ffa8086794
Oscar Nevarez <fu.wire@gmail.com>
2021-04-30 09:42:35 -0500

    Add https constraint only when needed

--- build.go
+++ build.go
@@ -5,6 +5,7 @@ import (
 	"github.com/paketo-buildpacks/php-web/procmgr"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/Masterminds/semver"
@@ -73,81 +74,80 @@ func Build(entries EntryResolver, dependencies DependencyService, clock chronos.
 			}
 			roadRunnerLayer.Launch, _ = entries.MergeLayerTypes("road-runner", context.Plan.Entries)
 
-			//if !strings.HasPrefix(dependency.URI, "https://") {
-			//	return packit.BuildResult{}, err
-			//}
+			if strings.HasPrefix(dependency.URI, "https://") {
 
-			logger.Subprocess("Installing RoadRunner Server %s", dependency.Version)
-			duration, err := clock.Measure(func() error {
-				platformPath, _ := os.MkdirTemp("", "platform")
-				return dependencies.Deliver(dependency, context.CNBPath, roadRunnerLayer.Path, platformPath)
+				logger.Subprocess("Installing RoadRunner Server %s", dependency.Version)
+				duration, err := clock.Measure(func() error {
+					platformPath, _ := os.MkdirTemp("", "platform")
+					return dependencies.Deliver(dependency, context.CNBPath, roadRunnerLayer.Path, platformPath)
 
-			})
-
-			if err != nil {
-				return packit.BuildResult{}, err
-			}
-
-			logger.Break()
-			logger.Action("Completed in %s", duration.Round(time.Millisecond))
-
-			//// --------------
-			logger.Subprocess("Downloading RoadRunner Server %s", dependency.URI)
-			duration, err = clock.Measure(func() error {
-
-				return RunProcs(procmgr.Procs{
-					Processes: map[string]procmgr.Proc{
-						"downloadRoadRunner": {
-							Command: "curl",
-							Args: []string{"-o",
-								"-v",
-								dependency.URI,
-								filepath.Join(roadRunnerLayer.Path, "roadrunner.tar.gz")},
-						},
-					},
 				})
 
-			})
+				if err != nil {
+					return packit.BuildResult{}, err
+				}
 
-			if err != nil {
-				fmt.Printf("Error: %s\n", err)
-				return packit.BuildResult{}, err
+				logger.Break()
+				logger.Action("Completed in %s", duration.Round(time.Millisecond))
+
+				//// --------------
+				logger.Subprocess("Downloading RoadRunner Server %s", dependency.URI)
+				duration, err = clock.Measure(func() error {
+
+					return RunProcs(procmgr.Procs{
+						Processes: map[string]procmgr.Proc{
+							"downloadRoadRunner": {
+								Command: "curl",
+								Args: []string{"-o",
+									"-v",
+									dependency.URI,
+									filepath.Join(roadRunnerLayer.Path, "roadrunner.tar.gz")},
+							},
+						},
+					})
+
+				})
+
+				if err != nil {
+					fmt.Printf("Error: %s\n", err)
+					return packit.BuildResult{}, err
+				}
+				logger.Break()
+				logger.Action("Completed in %s", duration.Round(time.Millisecond))
+				//// --------------
+
+				//logger.Subprocess("Building RoadRunner Server %s", dependency.Version)
+				//
+				//dir := fmt.Sprintf("%s", filepath.Join(roadRunnerLayer.Path))
+				//
+				////Check if install succeeded and source path is available for build
+				//if _, derr := os.Stat(dir); os.IsNotExist(derr) {
+				//	log.Println(derr)
+				//	return packit.BuildResult{}, derr
+				//} else {
+				//	buildDuration, prerr := clock.Measure(func() error {
+				//
+				//		// Run make to build RoadRunner specifying the directory (-C)
+				//		return RunProcs(procmgr.Procs{
+				//			Processes: map[string]procmgr.Proc{
+				//				"buildRoadRunner": {
+				//					Command: "make",
+				//					Args:    []string{"-C", dir},
+				//				},
+				//			},
+				//		})
+				//
+				//	})
+				//
+				//	if prerr != nil {
+				//		log.Println(derr)
+				//		return packit.BuildResult{}, derr
+				//	}
+				//
+				//	logger.Break()
+				//	logger.Action("Built in %s", buildDuration.Round(time.Millisecond))
+				//}
 			}
-			logger.Break()
-			logger.Action("Completed in %s", duration.Round(time.Millisecond))
-			//// --------------
-
-			//logger.Subprocess("Building RoadRunner Server %s", dependency.Version)
-			//
-			//dir := fmt.Sprintf("%s", filepath.Join(roadRunnerLayer.Path))
-			//
-			////Check if install succeeded and source path is available for build
-			//if _, derr := os.Stat(dir); os.IsNotExist(derr) {
-			//	log.Println(derr)
-			//	return packit.BuildResult{}, derr
-			//} else {
-			//	buildDuration, prerr := clock.Measure(func() error {
-			//
-			//		// Run make to build RoadRunner specifying the directory (-C)
-			//		return RunProcs(procmgr.Procs{
-			//			Processes: map[string]procmgr.Proc{
-			//				"buildRoadRunner": {
-			//					Command: "make",
-			//					Args:    []string{"-C", dir},
-			//				},
-			//			},
-			//		})
-			//
-			//	})
-			//
-			//	if prerr != nil {
-			//		log.Println(derr)
-			//		return packit.BuildResult{}, derr
-			//	}
-			//
-			//	logger.Break()
-			//	logger.Action("Built in %s", buildDuration.Round(time.Millisecond))
-			//}
 
 			roadRunnerLayer.Metadata = map[string]interface{}{
 				"built_at":  clock.Now().Format(time.RFC3339Nano),
