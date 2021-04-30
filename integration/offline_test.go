694ff194d87e488e823422c05763d7b650965c3b
Oscar Nevarez <fu.wire@gmail.com>
2021-04-30 12:25:43 -0500

    Fix typo

--- integration/offline_test.go
+++ integration/offline_test.go
@@ -54,7 +54,7 @@ func testOffline(t *testing.T, when spec.G, it spec.S) {
 
 			image, _, err = pack.Build.
 				WithPullPolicy("never").
-				WithBuildpacks(offlinegoBuildpack, httpdBuildpack).
+				WithBuildpacks(goBuildpack, httpdBuildpack).
 				WithNetwork("none").
 				Execute(name, source)
 			Expect(err).NotTo(HaveOccurred())
